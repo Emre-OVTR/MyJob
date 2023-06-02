f32b084fb92876472b8413da015086790ba2f94c
Emre Turhal <emreturhal9@gmail.com>
2023-06-02 20:56:19 +0200

    configured bottom navview

--- app/src/main/java/com/example/myjob/ui/MainActivity.kt
+++ app/src/main/java/com/example/myjob/ui/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.myjob.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.myjob.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configureBottomNavBar()
+    }
+
+    private fun configureBottomNavBar(){
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavView)
+        val navController = findNavController(R.id.fragNavHost)
+        bottomNavigationView.setupWithNavController(navController)
     }
 
 }
