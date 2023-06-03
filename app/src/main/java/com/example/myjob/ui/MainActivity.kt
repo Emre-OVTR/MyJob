dfee8c9e84b1f269a115b16075c9d6db20aab2f3
Emre Turhal <emreturhal9@gmail.com>
2023-06-03 02:17:30 +0200

    choosed viewbinding for mainactivity references

--- app/src/main/java/com/example/myjob/ui/MainActivity.kt
+++ app/src/main/java/com/example/myjob/ui/MainActivity.kt
@@ -8,22 +8,25 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.myjob.R
+import com.example.myjob.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var activityMainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val rootView = activityMainBinding.root
+        setContentView(rootView)
         configureBottomNavBar()
     }
 
     private fun configureBottomNavBar(){
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavView)
         val navController = findNavController(R.id.fragNavHost)
-        bottomNavigationView.setupWithNavController(navController)
+        activityMainBinding.bottomNavView.setupWithNavController(navController)
     }
 
 }
