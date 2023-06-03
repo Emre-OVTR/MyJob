07e9778fff5cd24e316b25c4d09413c5541313bd
Emre Turhal <emreturhal9@gmail.com>
2023-06-03 12:12:02 +0200

    fixed warnings

--- app/src/main/java/com/example/myjob/ui/MainActivity.kt
+++ app/src/main/java/com/example/myjob/ui/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.myjob.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.myjob.R
 import com.example.myjob.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/myjob/ui/addview/AddJobAppFragment.kt
+++ app/src/main/java/com/example/myjob/ui/addview/AddJobAppFragment.kt
@@ -1,11 +1,10 @@
 package com.example.myjob.ui.addview
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.myjob.R
+import androidx.fragment.app.Fragment
 import com.example.myjob.databinding.FragmentJobAppListBinding
 
 
@@ -21,11 +20,9 @@ class AddJobAppFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
        return fragmentJobAppListBinding.root
     }
 
-    companion object {
-
-    }
+    companion object
 }
