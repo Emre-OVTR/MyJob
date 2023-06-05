bc9b4213b5bedd5218b79219cacc77aa94000b72
Emre Turhal <emreturhal9@gmail.com>
2023-06-05 15:38:05 +0200

    adjusted toolbar for Top Level destinations

--- app/src/main/java/com/example/myjob/ui/MainActivity.kt
+++ app/src/main/java/com/example/myjob/ui/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.myjob.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.myjob.R
 import com.example.myjob.databinding.ActivityMainBinding
@@ -16,13 +19,18 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         val rootView = activityMainBinding.root
         setContentView(rootView)
-        configureBottomNavBar()
+        setSupportActionBar(activityMainBinding.myToolbar)
+        configureViews()
     }
 
-    private fun configureBottomNavBar(){
+    private fun configureViews(){
 
         val navController = findNavController(R.id.fragNavHost)
         activityMainBinding.bottomNavView.setupWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.list_fragment, R.id.map_fragment, R.id.add_job_fragment))
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragNavHost) as NavHostFragment
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
     }
 
 }
--- app/src/main/java/com/example/myjob/ui/jobappsview/JobAppListFragment.kt
+++ app/src/main/java/com/example/myjob/ui/jobappsview/JobAppListFragment.kt
@@ -21,7 +21,7 @@ class JobAppListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
+
         return fragmentJobAppListBinding.root
     }
 
