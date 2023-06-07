7095467a9a60348a740e58e8f353b3a448b41999
Emre Turhal <emreturhal9@gmail.com>
2023-06-07 23:59:43 +0200

    configured google map in mapFragment

--- app/src/main/java/com/example/myjob/ui/mapview/MapFragment.kt
+++ app/src/main/java/com/example/myjob/ui/mapview/MapFragment.kt
@@ -5,25 +5,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myjob.R
 import com.example.myjob.databinding.FragmentMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var fragmentMapBinding: FragmentMapBinding
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fragmentMapBinding = FragmentMapBinding.inflate(layoutInflater)
+        configureGoogleMap()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         return fragmentMapBinding.root
     }
 
     companion object
+
+ private fun configureGoogleMap(){
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap : GoogleMap) {
+        mMap = googleMap
+
+
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 }
